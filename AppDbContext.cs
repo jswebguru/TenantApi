8a61b9c12bead49d536a8a67915ec2848187b611
Guru <jswebguru@hotmail.com>
2024-07-10 21:33:30 +0900

    : fit MultiTenantContext error

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,11 +1,18 @@
 ﻿using Finbuckle.MultiTenant;
+using Finbuckle.MultiTenant.Abstractions;
+using Finbuckle.MultiTenant.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
 
-public class AppDbContext : MultiTenantDbContext<TenantInfo>
+public class AppDbContext : MultiTenantContext<TenantInfo>
 {
-    public AppDbContext(TenantInfo tenantInfo, DbContextOptions<AppDbContext> options)
-        : base(tenantInfo, options)
-    { }
+    public AppDbContext(ITenantInfo tenantInfo, DbContextOptions<AppDbContext> options)
+        : base(
+            tenantInfo,
+            options)
+    {
+    }
 
     public DbSet<TenantModel> TenantModels { get; set; }
 }
